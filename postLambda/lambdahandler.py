ec841849292d62943742095e2f360add152885ab
Mike Mackrory <mike@echovue.com>
2021-03-22 00:05:35 -0700

    Updates

--- postLambda/lambdahandler.py
+++ postLambda/lambdahandler.py
@@ -1,10 +1,20 @@
 import boto3
+import json
+
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('pets')
-    table.put_item(
+    table = dynamodb.Table('Pets')
+    print(event)
+    response = table.put_item(
         Item={
-            event
+            'id': event['id'],
+            'name': event['name'],
+
         }
     )
+    return response
+#    {
+#        'statusCode': 200,
+#        'body': json.dumps('Hello from Lambda!')
+#    }
