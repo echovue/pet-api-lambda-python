6adf9ec030be4056bc5fb16a1ad8565603a82ce4
Mike Mackrory <mike@echovue.com>
2021-03-22 00:41:42 -0700

    Updates

--- PetLambda_Set/index.py
+++ PetLambda_Set/index.py
@@ -1,20 +1,20 @@
 import boto3
 import json
 
-
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('Pets')
-    print(event)
     response = table.put_item(
         Item={
             'id': event['id'],
             'name': event['name'],
-
+            'breed': event['breed'],
+            'gender': event['gender'],
+            'owner': event['owner'],
+            'birthday': event['birthday']
         }
     )
-    return response
-#    {
-#        'statusCode': 200,
-#        'body': json.dumps('Hello from Lambda!')
-#    }
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
